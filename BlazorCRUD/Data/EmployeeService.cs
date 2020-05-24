c3586d387f18e0f4de9a5978dca92a0d07b5cf92
jamesrobertleadbetter <jamesrobertleadbetter@gmail.com>
2020-05-24 13:04:05 +0100

    Add project files.

---
+++ BlazorCRUD/Data/EmployeeService.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data
+{
+    public class EmployeeService : IEmployeeService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EmployeeService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        //Create
+        public async Task<string> Create(EmployeeInfo employee)
+        {
+            _context.Employees.Add(employee);
+
+            await _context.SaveChangesAsync();
+
+            return "Save Succsessfull";
+        }
+
+        //Read - get all employee
+        public List<EmployeeInfo> GetEmployees()
+        {
+            return _context.Employees.ToList();
+        }
+
+        //Read - get employee by id
+        public async Task<EmployeeInfo> GetEmployeeById(int id)
+        {
+            return await _context.Employees.FirstOrDefaultAsync(x => x.EmployeeId == id);
+        }
+
+        //Update
+        public async Task<string> UpdateEmployee(EmployeeInfo employee)
+        {
+            _context.Employees.Update(employee);
+
+            await _context.SaveChangesAsync();
+
+            return "Update Successfull";
+        }
+
+        //Delete
+        public async Task<string> DeleteEmployee(EmployeeInfo employee)
+        {
+            _context.Employees.Remove(employee);
+
+            await _context.SaveChangesAsync();
+
+            return "Delete Succsessfull";
+        }
+    }
+}
---
+++ BlazorCRUD/Data/IEmployeeService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace BlazorCRUD.Data
+{
+    public interface IEmployeeService
+    {
+        Task<string> Create(EmployeeInfo employee);
+        Task<string> DeleteEmployee(EmployeeInfo employee);
+        Task<EmployeeInfo> GetEmployeeById(int id);
+        List<EmployeeInfo> GetEmployees();
+        Task<string> UpdateEmployee(EmployeeInfo employee);
+    }
+}
